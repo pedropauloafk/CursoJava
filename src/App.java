3e195cfb47796ea74c4e457ff4548681b8968418
pedropauloafk <pedropaulo.afk@gmail.com>
2023-05-16 10:56:07 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }        
+        }
+
+
+        
+    }
+}
